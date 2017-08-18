7b1d69bda50fb7d05ca489499009e091075efa2d
FabioPi <31075106+FabioPi@users.noreply.github.com>
2017-08-18 16:17:31 +0200

    Add files via upload

---
+++ FormE.ini.php
@@ -0,0 +1,10 @@
+<?php
+define("HOST"  , "localhost");   
+define("NAMEDB", "FormE");
+define("USER"  , "root");
+define("PASSW" , "mysql");
+
+define("CRYPTO_KEY", "personalize_here_your_crypto_key");    //personalize_here_your_crypto_key
+
+define("UPLOAD_FILE", "Upload");  //Default DIRECTORY x File Upload
+?>
